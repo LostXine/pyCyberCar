ac646270ec1a218b79cf3cf3218c94636febeaa2
Xine <lostxine@gmail.com>
2017-11-16 21:33:25 +0800

    update nrf24

--- run_nrf24.py
+++ run_nrf24.py
@@ -6,11 +6,35 @@
 from nrf24 import NRF24
 import time
 from time import gmtime, strftime
+import threading, sys
 
+byteNum = 0
 
-pipes = [0xf0, 0xf0, 0xf0, 0xf0, 0xe1]
+def cal_unit(byte):
+    unitList = ["B","kB","MB","GB"]
+    num = len(unitList)
+    unit = 0
+    while unit < num and byte > 768:
+        unit += 1
+        byte /= 1024 
+    return (byte, unitList[unit])
+
+def cal_speed():
+    global byteNum
+    timeInv = 0.5
+    while True:
+        speed = byteNum / timeInv
+        byteNum = 0      
+        f = sys.stdout
+        f.write("Current speed: %.2f %s/s" % cal_unit(speed))
+        f.flush()
+        time.sleep(timeInv)
+        f.write('\r')
+    
 
 def run_nrf24():
+    global byteNum
+    pipes = [0xf0, 0xf0, 0xf0, 0xf0, 0xe1]
     print "----------------NRF24 Init-----------------"
     radio = NRF24()
     radio.begin(0, 0,25,18) #set gpio 25 as CE pin
@@ -24,16 +48,20 @@ def run_nrf24():
     
     print "----------------NRF24 Start-----------------"
     radio.startListening()
+    t = threading.Thread(target=cal_speed)
+    t.setDaemon(True)
+    t.start()
     try:
         while True:
             while not radio.available(pipes, True):
                 time.sleep(1000/1000000.0)
             recv_buffer = []
             radio.read(recv_buffer)
+            byteNum += len(recv_buffer)
     except:
-        pass
+        pass        
     finally:
-        print "----------------NRF24 Interrupted-----------------"
+        print "\n----------------NRF24 Interrupted-----------------"
         radio.stopListening()
         radio.end()
         print "----------------NRF24 Ended-----------------"
