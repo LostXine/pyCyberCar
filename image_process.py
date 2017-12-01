bfc183819a4def46835a4c63504143ab17d3eff0
Xine <lostxine@gmail.com>
2017-12-01 19:41:03 +0800

    add image process

--- image_process.py
+++ image_process.py
@@ -2,7 +2,47 @@
 # -*- coding: utf-8 -*-
 
 from config import *
+from driver import driver
+import cv2
 
-class image_process:
+class dip:
+    __car = None
+
+    def process_image(self, image):
+        # get timestamp do not change
+        uid = time.time()
+        
+        # --- image processing part --- 
+        # show image
+        cv2.imshow("Frame", image)
+        # waitkey
+        key = cv2.waitKey(10) & 0xff
+
+        # --- CyberCar controller part --- 
+        # how to control the car:
+        # set uid + set motor / steering
+
+        # Motor control:
+        # forward  0 -> 1.0
+        # backward 0 -> -1.0
+                
+        # Servo control:
+        # left - mid -right
+        # -1.0 - 0.0 - 1.0
+        if self.__car.setStatus(uid, motor=0.0, servo=0.0):
+            print "setStatus() failed."
+
+        # --- returning part
+        # return something not 0 to finish the code
+        # check key
+        if key == ord("q") or key == 27:
+            return 1
+        return 0
+
+    def __init__(self):
+        self.__car = driver()
+
+    def __del__(self):
+        del self.__car
     
 
--- run_cybercar.py
+++ run_cybercar.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from driver import driver
+from image_process import *
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
@@ -16,43 +17,20 @@ def run_cybercar():
     # prepare memery
     raw = PiRGBArray(c, c.resolution)
     # init car driver
-    d = driver()
+    d = dip()
     try:
         for frame in c.capture_continuous(raw, format='bgr', use_video_port=True):
-            # get timestamp
-            uid = time.time()
-            # grab image for opencv
             image = frame.array
-            # show image
-            cv2.imshow("Frame", image)
-            # waitkey
-            key = cv2.waitKey(10) & 0xff
+            if d.process_image(image):
+                break;
             # reset
             raw.truncate(0)
-            # check key
-            if key == ord("q") or key == 27:
-                break
-
-            # how to control the car:
-
-            # set uid + set motor / steering
-
-            # Motor control:
-            # forward  0 -> 1.0
-            # backward 0 -> -1.0
-            
-            # Servo control:
-            # left - mid -right
-            # -1.0 - 0.0 - 1.0
-            if d.setStatus(uid, motor=0.0, servo=0.0):
-                print "setStatus() failed."
-
-        return 0
     except KeyboardInterrupt:
-        return 0
+        pass
     except Exception, e:
         print repr(e)
         return 1
+    return 0
                
     
 if __name__ == '__main__':
